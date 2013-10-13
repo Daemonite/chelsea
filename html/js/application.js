191d6c433a9f1164115582a3a3d09edca3402fbb
MAYA Ma <sesemaya@gmail.com>
2013-10-14 09:21:13 +1100

    Secondary nav fixes
    Fixed some javascript issues related to the secondary navigation.

--- html/js/application.js
+++ html/js/application.js
@@ -1,47 +1,42 @@
 $(document).ready(function(){
   //for typography examples only
-  $('.well address:last-child,.well blockquote:last-child,.well ol:last-child,.well p:last-child,.well table:last-child,.well ul:last-child,.well .alert:last-child').addClass('last-child')
+  $('.well address:last-child,.well blockquote:last-child,.well ol:last-child,.well p:last-child,.well table:last-child,.well ul:last-child,.well .alert:last-child').addClass('last-child');
 
 
 
   //nav side affix
-  if($('body').has('.nav-side').length > 0){
-    var timer
-    navsideaffix()
-    $(window).bind('resize',function(){
-      timer && clearTimeout(timer)
-      timer = setTimeout(onresize,100)
-    })
-    function onresize(){
-      navsideaffix()
-    }
-  }
+  var $navside = $('.nav-side');
+  function onResize(c,t){
+    onresize = function(){
+      clearTimeout(t);
+      t = setTimeout(c,100)
+    };
+    return c;
+  };
+  onResize(function(){
+    navsideaffix();
+  })();
   function navsideaffix(){
-    var $navside = $('.nav-side .nav'),
-        navsideoffset = $navside.offset().top
-    $navside.affix({
-      offset:{
-        top:navsideoffsetcal(navsideoffset),
-        bottom:233
-      }
-    })
-  }
-  function navsideoffsetcal(offset){
-    if($(window).width() < 980)
-      return offset -= 30
-    else
-      return offset -= 70
+    if($navside.length && $(window).width() > 979){
+      var navsideoffset = $navside.offset().top - 41;
+      $navside.affix({
+        offset:{
+          top:navsideoffset,
+          bottom:233
+        }
+      });
+    }
   }
 
   //navbar dropdown
-  $('.navbar .nav > li > ul').addClass('dropdown-menu')
+  $('.navbar .nav > li > ul').addClass('dropdown-menu');
 
   //logo bottom margin fix
-  $('.logo h1:last-child').addClass('last-child')
+  $('.logo h1:last-child').addClass('last-child');
 
   //tooltip
-  $('[rel=tooltip]').tooltip()
+  $('[rel=tooltip]').tooltip();
 
   //placeholder
-  $('input[placeholder]').placeholder()
+  $('input[placeholder]').placeholder();
 })
